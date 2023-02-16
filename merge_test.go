0f27c085e492794483cda5fbae5466df6adf489c
Rafal P | Raftech <rafal.pieniazek@raftech.nl>
2023-02-16 10:43:21 +0100

    modify unit tests

--- merge_test.go
+++ merge_test.go
@@ -30,19 +30,19 @@ providers:
         auth:
             ssh_key: ssh:key:2312312
         providerType: github
-`), strings.TrimSpace(result))
+`), strings.TrimSpace(string(result)))
 }
 
 func TestMergeYAML_MissingFile(t *testing.T) {
 	result, err := MergeYAML("tests/test1.yaml", "tests/test3.yaml")
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "no such file or directory")
-	assert.Equal(t, "", result)
+	assert.Equal(t, "", string(result))
 }
 
 func TestMergeYAML_InvalidYAML(t *testing.T) {
 	result, err := MergeYAML("tests/test1.yaml", "tests/invalid.yaml")
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "failed to unmarshal data from file")
-	assert.Equal(t, "", result)
+	assert.Equal(t, "", string(result))
 }
